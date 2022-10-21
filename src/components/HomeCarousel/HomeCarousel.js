1d6089b4a33ee32f555811764b8ff4abde7547d4
risingtalent0609 <bsbussiness000@gmail.com>
2022-10-21 16:11:53 +0900

    success

--- src/components/HomeCarousel/HomeCarousel.js
+++ src/components/HomeCarousel/HomeCarousel.js
@@ -13,68 +13,70 @@ const HomeCarousel = () => {
 
   return (
     <>
-      <Carousel
-        activeIndex={index}
-        onSelect={handleSelect}
-        className="carousel-container mt-[100px]"
-      >
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="/slide/slide1.webp"
-            alt="First slide"
-          />
+      <div className="container">
+        <Carousel
+          activeIndex={index}
+          onSelect={handleSelect}
+          className="carousel-container mt-[100px]"
+        >
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/slide/slide1.webp"
+              alt="First slide"
+            />
 
-          <Link>
-            <div className="banner-description">
-              <img
-                src="/slide/esg-reporting.jpg"
-                width="150px"
-                alt="esg reporting"
-              />
-              What is ESG Reporting? <br /> And why should it be your
-              organization's new focus?
-            </div>
-          </Link>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="/slide/slide2.webp"
-            alt="Second slide"
-          />
+            <Link>
+              <div className="banner-description">
+                <img
+                  src="/slide/esg-reporting.jpg"
+                  width="150px"
+                  alt="esg reporting"
+                />
+                What is ESG Reporting? <br /> And why should it be your
+                organization's new focus?
+              </div>
+            </Link>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/slide/slide2.webp"
+              alt="Second slide"
+            />
 
-          <Link>
-            <div className="banner-description">
-              <img
-                src="/slide/tree-planting.jpg"
-                width="150px"
-                alt="tree planting"
-              />
-              Do you know you can EARN while doing sustainable activities?{" "}
-              <br />
-              You can click here to find an activity near you today.
-            </div>
-          </Link>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="/slide/slide3.webp"
-            alt="Third slide"
-          />
-          <Link>
-            <div className="banner-description">
-              <img
-                src="/slide/web3-business.png"
-                width="150px"
-                alt="web3 business"
-              />
-              How your business can use WEB3?
-            </div>
-          </Link>
-        </Carousel.Item>
-      </Carousel>
+            <Link>
+              <div className="banner-description">
+                <img
+                  src="/slide/tree-planting.jpg"
+                  width="150px"
+                  alt="tree planting"
+                />
+                Do you know you can EARN while doing sustainable activities?{" "}
+                <br />
+                You can click here to find an activity near you today.
+              </div>
+            </Link>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/slide/slide3.webp"
+              alt="Third slide"
+            />
+            <Link>
+              <div className="banner-description">
+                <img
+                  src="/slide/web3-business.png"
+                  width="150px"
+                  alt="web3 business"
+                />
+                How your business can use WEB3?
+              </div>
+            </Link>
+          </Carousel.Item>
+        </Carousel>
+      </div>
       <div className="uponImage">
         <img src="/site_logo.webp" alt="site logo" />
       </div>
