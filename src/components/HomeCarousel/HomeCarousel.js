e39b30f94182c691e48c71f110817fb9b9c28202
risingtalent0609 <bsbussiness000@gmail.com>
2022-10-21 15:58:26 +0900

    delete caption

--- src/components/HomeCarousel/HomeCarousel.js
+++ src/components/HomeCarousel/HomeCarousel.js
@@ -23,17 +23,18 @@ const HomeCarousel = () => {
           src="/slide/slide1.webp"
           alt="First slide"
         />
-        <Carousel.Caption className="text-center">
-          <h3>Sustainability Made Easy</h3>
-          <p>
-            <img src="/site_logo.webp" alt="site logo" />
-          </p>
-        </Carousel.Caption>
+
         <Link>
-        <div className="banner-description">
-          <img src="/slide/esg-reporting.jpg" width="150px" alt="esg reporting" />
-          What is ESG Reporting? <br /> And why should it be your organization's new focus?
-        </div></Link>
+          <div className="banner-description">
+            <img
+              src="/slide/esg-reporting.jpg"
+              width="150px"
+              alt="esg reporting"
+            />
+            What is ESG Reporting? <br /> And why should it be your
+            organization's new focus?
+          </div>
+        </Link>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -42,18 +43,17 @@ const HomeCarousel = () => {
           alt="Second slide"
         />
 
-        <Carousel.Caption className="text-center">
-          <h3>Sustainability Made Easy</h3>
-          <img src="/site_logo.webp" alt="site logo" />
-        </Carousel.Caption>
-
         <Link>
           <div className="banner-description">
-            <img src="/slide/tree-planting.jpg" width="150px" alt="tree planting"/>
-          Do you know you can EARN while doing sustainable activities? <br />
-          You can click here to find an activity near you today.
-        </div></Link>
-
+            <img
+              src="/slide/tree-planting.jpg"
+              width="150px"
+              alt="tree planting"
+            />
+            Do you know you can EARN while doing sustainable activities? <br />
+            You can click here to find an activity near you today.
+          </div>
+        </Link>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -62,16 +62,16 @@ const HomeCarousel = () => {
           alt="Third slide"
         />
 
-        <Carousel.Caption className="text-center">
-          <h3>Sustainability Made Easy</h3>
-          <img src="/site_logo.webp" alt="site logo" />
-        </Carousel.Caption>
-
         <Link>
           <div className="banner-description">
-            <img src="/slide/web3-business.png" width="150px" alt="web3 business"/>
-          How your business can use WEB3?
-        </div></Link>
+            <img
+              src="/slide/web3-business.png"
+              width="150px"
+              alt="web3 business"
+            />
+            How your business can use WEB3?
+          </div>
+        </Link>
       </Carousel.Item>
     </Carousel>
   );
