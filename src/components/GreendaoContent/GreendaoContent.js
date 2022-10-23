de109cf91c399fb0b08a3b7a804c5b36cccf8276
risingtalent0609 <bsbussiness000@gmail.com>
2022-10-24 05:16:23 +0900

    sd

--- src/components/GreendaoContent/GreendaoContent.js
+++ src/components/GreendaoContent/GreendaoContent.js
@@ -1,28 +1,42 @@
 import React from "react";
+import { useState } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 
 import "./GreendaoContent.scss";
 
 const Greendao_content = () => {
+  const [summary, useSummary] = useState(false);
+  const clickSummary = () => {
+    useSummary(!summary);
+  };
   return (
     <Container>
       <Row>
         <Col>
           <h2>What is GreenDAO(GD)</h2>
+          <div onClick={clickSummary}>Summary</div>
+          {summary ? (
+            <p>
+              GreenDAO is a platform that incentivises participation in ESG
+              activities, bridging event organisers with participants and
+              appropriate funding. <br />
+              GreenDAO aims to make sustainability accessible to everyone within
+              each of their various capacities. The platform will qualify and
+              quantify carbon-related activities, as well as, non-carbon related
+              activities such as social philanthropy, equality and conservation.
+              The Decentralised Autonomous Organisation (DAO) serves to create a
+              consensus network to determine the value of various non-carbon
+              related activities, and to create a erse and international network
+              for like-minded individuals in the ustainability space to share
+              information and collaborate.
+            </p>
+          ) : (
+            <></>
+          )}
           <p>
-            - GreenDAO is a platform that incentivises participation in ESG
-            activities, bridging event organisers with participants and
-            appropriate funding. <br />
-            GreenDAO aims to make sustainability accessible to everyone within
-            each of their various capacities. The platform will qualify and
-            quantify carbon-related activities, as well as, non-carbon related
-            activities such as social philanthropy, equality and conservation.
-            The Decentralised Autonomous Organisation (DAO) serves to create a
-            consensus network to determine the value of various non-carbon
-            related activities, and to create a erse and international network
-            for like-minded individuals in the ustainability space to share
-            information and collaborate.
+            -
             <br />
+            <div></div>
             Who does it serve
             <br />- GreenDAO serves both individuals and businesses. By breaking
             down any and all barriers for anyone who wants to engage in
