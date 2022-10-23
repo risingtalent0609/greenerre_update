1f9857f017530c39eda9a3c00713c844fc5879d6
risingtalent0609 <bsbussiness000@gmail.com>
2022-10-23 18:32:11 +0900

    ss

--- src/components/EcosystemItem/EcosystemItem.js
+++ src/components/EcosystemItem/EcosystemItem.js
@@ -7,12 +7,12 @@ const EcosystemItem = (props) => {
     <div className="item-control">
       {props.type === "local" ? (
         <Link to={props.alink}>
-          <img src={props.image} alt="item" />
+          <img src={props.image} alt="item" className="my_pic" />
           <h4 className="text-center">{props.text}</h4>
         </Link>
       ) : (
         <a href={props.alink}>
-          <img src={props.image} alt="item" />
+          <img src={props.image} alt="item" className="my_pic" />
           <h4 className="text-center">{props.text}</h4>
         </a>
       )}
